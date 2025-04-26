772b322fcf8ba569be63a4e21d4dea3b4668775e
Pavathanya <pavathanyamanoharan@gmail.com>
2025-04-26 12:25:40 +0530

    learing plan submit button issue

--- frontend/src/Pages/CreateLearningPlan.jsx
+++ frontend/src/Pages/CreateLearningPlan.jsx
@@ -198,10 +198,7 @@ const CreateLearningPlan = () => {
               />
             </div>
 
-            <button
-                type="submit"
-                className="w-full mt-6 px-4 py-2 text-sm font-medium text-white bg-green-600 rounded-md shadow hover:bg-green-700"
-            >
+            
               {editLearningPlans ? "Update Learning Plan" : "Create Learning Plan"}
             </button>
             <button
